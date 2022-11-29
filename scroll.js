dc319b0fae3754246aa80039374182e2ff51b15d
Kenan <exonautprime@gmail.com>
2022-11-29 14:31:09 +0100

    za ne daj boze

--- scroll.js
+++ scroll.js
@@ -1,19 +1,12 @@
-var bar = document.getElementById("dropdown-nav")
 
+var nav = document.getElementById("navbar-scroll");
 
+window.onscroll = function() {scrollFunction()};
 
-window.onscroll = myScroll(){
-
-if(document.body.scrollTop >800 || document.documentElement.scrollTop > 800){
-bar.
-
-
-}
-
-
-
-}
-
-
-
-
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    nav.style.top = "0";
+  } else {
+    nav.style.top = "-50px";
+  }
+} 
