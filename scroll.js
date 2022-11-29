144b8c2483b54d8102c0cfae638cd20952a36568
Kenan <exonautprime@gmail.com>
2022-11-29 20:34:22 +0100

    Gave up on the complex navbar for now.

--- scroll.js
+++ scroll.js
@@ -7,6 +7,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
     nav.style.top = "0";
   } else {
-    nav.style.top = "-50px";
+    nav.style.top = "-60px";
   }
 } 
